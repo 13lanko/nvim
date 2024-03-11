68836a5efcf1c45d0c2aecfcf8c266c8c7a83394
Hau Zhang <zhanghau0512@gmail.com>
2024-03-11 23:03:55 +0100

    kleine Änderung wurde vorgenommen

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -6,21 +6,7 @@ return {
 		local config = require("catppuccin")
 		config.setup({
 			flavour = "mocha", -- latte, frappe, macchiato, mocha
-      --transparent_background = true, -- disables setting the background color.
-			color_overrides = {},
-			custom_highlights = {},
-			integrations = {
-				cmp = true,
-				gitsigns = true,
-				nvimtree = true,
-				treesitter = true,
-				notify = false,
-				mini = {
-					enabled = true,
-					indentscope_color = "",
-				},
-				-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-			},
+      transparent_background = true, -- disables setting the background color.
 		})
 		vim.cmd.colorscheme("catppuccin")
 	end,
