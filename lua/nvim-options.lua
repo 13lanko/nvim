0bda59c1139f40d0101c463b3de6b873eb2bbea6
Hau Zhang <zhanghau0512@gmail.com>
2024-03-22 01:16:38 +0100

    kleine Änderung

--- lua/nvim-options.lua
+++ lua/nvim-options.lua
@@ -6,7 +6,19 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 
-vim.keymap.set('n', '<leader>f', ':nohlsearch<CR>') --Suchergibnisse löschen
+vim.keymap.set("n", "<leader>f", ":nohlsearch<cr>") --suchergibnisse löschen
+vim.keymap.set({ "v", "n" }, "<leader>v", "<c-v>", { noremap = true })
+vim.keymap.set({ "v", "n" }, "<leader>tm", ":terminal<cr>", { noremap = true })
+vim.keymap.set({ "v", "n" }, "<leader>wj", "<c-W>j", { noremap = true })
+vim.keymap.set({ "v", "n" }, "<leader>wh", "<c-W>h", { noremap = true })
+vim.keymap.set({ "v", "n" }, "<leader>wk", "<c-W>k", { noremap = true })
+vim.keymap.set({ "v", "n" }, "<leader>wl", "<c-W>l", { noremap = true })
+vim.keymap.set({ "v", "n" }, "<leader>ww", "<c-W>w", { noremap = true })
+vim.keymap.set({ "v", "n" }, "<leader>wsh", ":split<cr>:bprev<cr><c-W>l", { noremap = true })
+vim.keymap.set({ "v", "n" }, "<leader>wsv", ":vsplit<cr>:bprev<cr><c-W>j", { noremap = true })
+vim.keymap.set({ "v", "n" }, "<c-i>", ":bprev<cr>", { noremap = true })
+vim.keymap.set({ "v", "n" }, "<c-o>", ":bnext<cr>", { noremap = true })
+
 
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
