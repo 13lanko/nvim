c34eef4c17bee53389d5581d6c31406cf7a95418
Hau Zhang <hau.zhang@tu-dortmund.de>
2024-03-24 21:30:04 +0100

    small changes

--- lua/nvim-options.lua
+++ lua/nvim-options.lua
@@ -16,10 +16,7 @@ vim.keymap.set({ "v", "n" }, "<leader>wl", "<c-W>l", { noremap = true })
 vim.keymap.set({ "v", "n" }, "<leader>ww", "<c-W>w", { noremap = true })
 vim.keymap.set({ "v", "n" }, "<leader>wsh", ":split<cr>:bprev<cr><c-W>l", { noremap = true })
 vim.keymap.set({ "v", "n" }, "<leader>wsv", ":vsplit<cr>:bprev<cr><c-W>j", { noremap = true })
-vim.keymap.set({ "v", "n" }, "<c-i>", ":bprev<cr>", { noremap = true })
-vim.keymap.set({ "v", "n" }, "<c-o>", ":bnext<cr>", { noremap = true })
+--vim.keymap.set({ "v", "n" }, "<c-i>", ":bprev<cr>", { noremap = true })
+--vim.keymap.set({ "v", "n" }, "<c-o>", ":bnext<cr>", { noremap = true })
 
 
-vim.g.loaded_python3_provider = 0
-vim.g.loaded_ruby_provider = 0
-vim.g.loaded_perl_provider = 0
