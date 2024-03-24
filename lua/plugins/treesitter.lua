ef7cf168c126f907e1997c82975e88c7cee3ea47
Hau Zhang <hau.zhang@tu-dortmund.de>
2024-03-24 18:40:56 +0100

    transparent function added

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,14 @@
-return  {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      autoinstall = true,
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-
-  end
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			autoinstall = true,
+			--ensure_installed = { "lua", "python" },
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
